d82a07c91502aedacb53dec35b4be588276fdd7e
Chauncey Thorn <cthorn@phase2technology.com>
2015-11-07 11:47:40 -0500

    Initial import of Demo site

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * Index file for ansible demo site.
+ */
+
+$current_build = 'dev';
+$site_name = 'Ansible Rolling deployment Demo';
+
+
+print '<h1>' . $site_name . '</h1>';
+print '<p>Current Version: ' . $current_build . '</p>';
+
