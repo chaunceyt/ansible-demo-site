5039f3a1ebf4e855eecfecc6c6ea8421ecbe9c59
Chauncey Thorn <cthorn@phase2technology.com>
2015-11-07 13:32:09 -0500

    Upgrading to 1.0.2

--- index.php
+++ index.php
@@ -4,10 +4,9 @@
  * Index file for ansible demo site.
  */
 
-$current_build = '1.0.1';
+$current_build = '1.0.2';
 $site_name = 'Ansible Rolling deployment Demo';
 
 
 print '<h1>' . $site_name . '</h1>';
 print '<p>Current Version: ' . $current_build . '</p>';
-phpinfo();
