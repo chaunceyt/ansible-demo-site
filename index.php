680a75cef3ba3137b81dfa94392e4b1c8c3d405e
Chauncey Thorn <cthorn@phase2technology.com>
2015-11-07 19:13:39 -0500

    Adding phpinfo()

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
  * Index file for ansible demo site.
  */
 
-$current_build = '1.0.3';
+$current_build = '1.0.4';
 $site_name = 'Ansible Rolling deployment Demo';
-
 
 print '<h1>' . $site_name . '</h1>';
 print '<p>Current Version: ' . $current_build . '</p>';
 print '<p>' . $_SERVER['SERVER_ADDR'] . '</p>';
+
+phpinfo();
