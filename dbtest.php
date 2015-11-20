3b3351a805bc6879ee6f35527879b9ec8bab31cd
Chauncey Thorn <cthorn@phase2technology.com>
2015-11-20 10:46:49 -0500

    Fixed error in dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -1,8 +1,8 @@
 <?php
 
 $db_host = 'p2-db1.dev';
-$db_user = 'drupal_user'
-$db_password = 'drupal_user'
+$db_user = 'drupal_user';
+$db_password = 'drupal_user';
 $db_name = 'drupal_db';
 $db = @mysqli_connect($db_host, $db_user, $db_password, $db_name) OR die ('Could not connect to MySQL: ' . mysqli_connect_error());
 $sql = "SHOW DATABASES";
